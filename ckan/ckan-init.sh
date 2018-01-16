6c7e82c00d21c0ba4abbe82d7aebe3d37fb1ffb2
Alessandro Parma <randomorder@users.noreply.github.com>
2018-01-16 17:11:58 +0100

    Update ckan-init.sh

--- ckan/ckan-init.sh
+++ ckan/ckan-init.sh
@@ -9,6 +9,9 @@ paster --plugin=ckan sysadmin add ckanadmin --config /etc/ckan/default/ckan.ini
 paster --plugin=ckan datastore set-permissions -c "/etc/ckan/default/ckan.ini" > "/tmp/set_permissions.sql"
 psql  -h db -p 5432 -U postgres -f "/tmp/set_permissions.sql"
 
+# Setup Multilang
+paster --plugin=ckanext-multilang multilangdb initdb --config=/etc/ckan/default/ckan.ini
+
 # Vocabulary Load
 paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/language/skos/languages-skos.rdf --name languages --config /etc/ckan/default/ckan.ini
 paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/data-theme/skos/data-theme-skos.rdf --name eu_themes --config /etc/ckan/default/ckan.ini
