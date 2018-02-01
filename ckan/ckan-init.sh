e282b2c18026341c3fc2220de0340fd2e811cb39
Alessandro Parma <alessa.parma@gmail.com>
2018-02-01 15:49:14 +0100

    update dcatapit config

--- ckan/ckan-init.sh
+++ ckan/ckan-init.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+EUROVOC_TO_THEMES_MAPPING_FILE="$CKAN_HOME/src/ckanext-dcatapit/examples/eurovoc_mapping.rdf"
+PATH_TO_EUROVOC="$CKAN_HOME/src/ckanext-dcatapit/examples/eurovoc.rdf"
+CKAN_INI_PAT="/etc/ckan/default/ckan.ini"
+
 # Add user "ckanadmin" with password "ckanpassword". Add user "ckanadmin" to sysadmin group. Change password at first login.
 #paster --plugin=ckan user remove ckanadmin --config /etc/ckan/default/ckan.ini
-paster --plugin=ckan user add ckanadmin email=admin@mail.com password=ckanpassword --config /etc/ckan/default/ckan.ini
-paster --plugin=ckan sysadmin add ckanadmin --config /etc/ckan/default/ckan.ini
+paster --plugin=ckan user add ckanadmin email=admin@mail.com password=ckanpassword --config "$CKAN_INI_PATH"
+paster --plugin=ckan sysadmin add ckanadmin --config "$CKAN_INI_PATH"
 
 # Setup Datastore
-paster --plugin=ckan datastore set-permissions -c "/etc/ckan/default/ckan.ini" > "/tmp/set_permissions.sql"
+paster --plugin=ckan datastore set-permissions -c "$CKAN_INI_PATH" > "/tmp/set_permissions.sql"
 psql  -h db -p 5432 -U postgres -f "/tmp/set_permissions.sql"
 
 # Vocabulary Load
-paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/language/skos/languages-skos.rdf --name languages --config /etc/ckan/default/ckan.ini
-paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/data-theme/skos/data-theme-skos.rdf --name eu_themes --config /etc/ckan/default/ckan.ini
-paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/place/skos/places-skos.rdf --name places --config /etc/ckan/default/ckan.ini
-paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/frequency/skos/frequencies-skos.rdf --name frequencies --config /etc/ckan/default/ckan.ini
-paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/file-type/skos/filetypes-skos.rdf --name filetype --config /etc/ckan/default/ckan.ini
+paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/language/skos/languages-skos.rdf --name languages --config "$CKAN_INI_PATH"
+paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/data-theme/skos/data-theme-skos.rdf --name eu_themes --config "$CKAN_INI_PATH"
+paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/place/skos/places-skos.rdf --name places --config "$CKAN_INI_PATH"
+paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/frequency/skos/frequencies-skos.rdf --name frequencies --config "$CKAN_INI_PATH"
+paster --plugin=ckanext-dcatapit vocabulary load --url http://publications.europa.eu/mdr/resource/authority/file-type/skos/filetypes-skos.rdf --name filetype --config "$CKAN_INI_PATH"
+wget https://raw.githubusercontent.com/italia/daf-ontologie-vocabolari-controllati/master/VocabolariControllati/Licenze/Licenze.rdf
+paster --plugin=ckanext-dcatapit vocabulary load --filename Licenze.rdf.1 --name licenses --config "$CKAN_INI_PATH"
+paster --plugin=ckanext-dcatapit vocabulary load --filename "$EUROVOC_TO_THEMES_MAPPING_FILE" --name subthemes --config "$CKAN_INI_PATH" "$PATH_TO_EUROVOC"
+
