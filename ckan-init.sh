76dc1c6a9979ca3138bba9e19ad50c50833496ea
Alessio Cimarelli <jenkin@dataninja.it>
2019-09-27 12:35:36 +0200

    Add harvesters orgs and sources.

--- ckan-init.sh
+++ ckan-init.sh
@@ -15,6 +15,36 @@ add_groups () {
   done
 }
 
+# Wait until CKAN REST APIs are ready, then create orgs from data/init/harvesters/orgs
+add_orgs () {
+  until $(curl --output /dev/null --silent --head --fail "${CKAN_SITE_URL}"); do
+    echo "CKAN is not ready, yet. Trying again in two seconds."
+    sleep 2
+  done
+
+  apikey=$(psql -q -t -h ${CKAN_DB_HOST} -U ckan -d ${CKAN_DB_USER} -c "select apikey from public.user where name='${CKAN_ADMIN_USERNAME}';")
+
+  for file in "${CKAN_HOME}"/data/init/harvesters/orgs/*.json; do
+   echo "Creating organization from file ${file}"
+   curl -i -H "X-CKAN-API-Key: ${apikey}" -XPOST -d @$file "${CKAN_SITE_URL}"/api/3/action/organization_create
+  done
+}
+
+# Wait until CKAN REST APIs are ready, then create sources from data/init/harvesters/sources
+add_sources () {
+  until $(curl --output /dev/null --silent --head --fail "${CKAN_SITE_URL}"); do
+    echo "CKAN is not ready, yet. Trying again in two seconds."
+    sleep 2
+  done
+
+  apikey=$(psql -q -t -h ${CKAN_DB_HOST} -U ckan -d ${CKAN_DB_USER} -c "select apikey from public.user where name='${CKAN_ADMIN_USERNAME}';")
+
+  for file in "${CKAN_HOME}"/data/init/harvesters/sources/*.json; do
+   echo "Creating source from file ${file}"
+   curl -i -H "X-CKAN-API-Key: ${apikey}" -XPOST -d @$file "${CKAN_SITE_URL}"/api/3/action/package_create
+  done
+}
+
 path_to_eurovoc="${CKAN_HOME}/src/ckanext-dcatapit/examples/eurovoc.rdf"
 config="${CKAN_CONFIG}/ckan.ini"
 
@@ -39,5 +69,9 @@ wget "https://raw.githubusercontent.com/italia/daf-ontologie-vocabolari-controll
 paster --plugin=ckanext-dcatapit vocabulary load --filename "/tmp/theme-subtheme-mapping.rdf" --name subthemes --config "${config}" "$path_to_eurovoc"
 
 add_groups
+if [ -d "${CKAN_HOME}/data/init/harvesters/" ]; then
+    add_orgs
+    add_sources
+fi
 
 echo -e "\nCKAN init completed successfully"
